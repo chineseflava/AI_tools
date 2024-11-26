5442531327fd08779615bdfaf5cf5b8c48d875a1
Björn Walter <bjornwa@axis.com>
2024-11-26 11:46:18 +0100

    Initialize frontend (again...)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
-// function App() {
-//   const [count, setCount] = useState(0)
+function App() {
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-import React from 'react';
-import './App.css';
-import MessageList from './components/Conversation';
-
-const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Message Management App</h1>
-      </header>
-      <main>
-        <MessageList />
-      </main>
-    </div>
+    <>
+      <Home />
+    </>
   );
-};
+}
 
-export default App;
+export default App
